22107585f7c1ea019c6a545886a37294252ef11b
guilherme <guilhermestrela@gmail.com>
2025-06-27 15:17:28 -0300

    refactor(ProjectsGrid): remove extra line break

--- src/assets/components/ProjectsGrid/index.tsx
+++ src/assets/components/ProjectsGrid/index.tsx
@@ -299,7 +299,6 @@ function ProjectGrid(){
                         </DivFlex>         
                     </DivFlex>
                 </DivFlex>
-
                 
             </DivFlex>
         </>
