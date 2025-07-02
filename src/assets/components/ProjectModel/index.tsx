e2cdf23bf39dca80b2bd87d8c4600d999f624f43
guilherme <guilhermestrela@gmail.com>
2025-07-02 18:04:45 -0300

    refactor(ProjectsGrid): Refactor project display
    - Created reusable ProjectModel component
    - Improved code structure and readability
    - Reduced code duplication
    - Enhanced maintainability
    - Improved responsiveness

---
+++ src/assets/components/ProjectModel/index.tsx
@@ -0,0 +1,140 @@
+import { DivContainerText, DivFlex, DivFlexImage, Img, Link, Texto, TitleH2 } from "../../../uiKit";
+
+interface ProjectModelProps {
+    foto: string;
+    titulo: string;
+    descricao: string;
+    techStack: string;      
+    iconPreview: string;
+    iconGit: string;
+    linkPreview: string;
+    linkGit: string;
+}
+
+function ProjectModel({ foto, titulo, descricao, techStack, iconPreview, iconGit, linkPreview, linkGit }: ProjectModelProps) {
+    return (
+        <>
+            <DivFlex 
+                        width="373px"                                                
+                        backgroundColor="#FFFFFF"
+                        flexdirection="column"
+                        paddingbottom="40px"  
+                        borderradius="10px"                      
+                        >
+                        <DivFlexImage>
+                            <Img
+                                borderradius="10px" 
+                                src={ foto }/>
+                        </DivFlexImage>
+                        <DivContainerText
+                            flexdirection="column"
+                            heightTotal="100px"
+                            gap="20px"
+                            alignitems="center"
+                            justifycontent="center"
+                            paddingtopmob="20px"
+                            heightmob="140px">
+                            <TitleH2
+                                color="#000000"
+                                fontsize="18px"
+                                fontweight="bold"
+                                fontfamily="Roboto"                        
+                                textalign="center">
+                                { titulo }
+                            </TitleH2>
+                            <Texto
+                                color="#666666"
+                                fontsize="16px"
+                                fontweight="400"
+                                fontfamily="Roboto"
+                                textalign="center"
+                                paddingbottommob="20px"
+                                >
+                                { descricao }
+                            </Texto>
+                    </DivContainerText>
+                    <DivContainerText
+                            flexdirection="row"
+                            gap="20px"
+                            alignitems="center"
+                            justifycontent="center"
+                            paddingtop="20px"
+                            paddingtopmob="20px">
+                            <TitleH2
+                                color="#000000"
+                                fontsize="14px"
+                                fontweight="bold"
+                                fontfamily="Roboto"                        
+                                textalign="center">
+                                Tech Stack:
+                            </TitleH2>
+                            <Texto
+                                color="#666666"
+                                fontsize="14px"
+                                fontweight="400"
+                                fontfamily="Roboto"
+                                textalign="center"
+                                >
+                                { techStack }
+                            </Texto>
+                    </DivContainerText>
+                        <DivFlex
+                            flexdirection="row"
+                            alignitems="center"
+                            justifycontent="center"
+                            gap="80px"
+                            >
+                            <DivContainerText
+                                flexdirection="row"
+                                gap="10px"
+                                alignitems="center"
+                                justifycontent="center"
+                                paddingtop="20px"                  
+                                >
+                                <Img 
+                                    width={"20px"} 
+                                    height={"20px"} 
+                                    alignitens="center"
+                                    src={ iconPreview }/>
+                                <Link 
+                                    fontsize="14px"
+                                    fontweight="400"
+                                    fontfamily="Roboto"
+                                    color="#000000"
+                                    href={ linkPreview } 
+                                    target="_blank"
+                                    >                               
+                                    Live Preview
+                                </Link>
+                            </DivContainerText>
+                            <DivContainerText
+                                flexdirection="row"
+                                gap="10px"
+                                alignitems="center"
+                                justifycontent="center"
+                                paddingtop="20px"                  
+                                >
+                                <Img 
+                                    width={"20px"} 
+                                    height={"20px"} 
+                                    alignitens="center"
+                                    src={ iconGit }/>
+                                <Link 
+                                    fontsize="14px"
+                                    fontweight="400"
+                                    fontfamily="Roboto"
+                                    color="#000000"
+                                    href={ linkGit}
+                                    target="_blank"
+                                    >                             
+                                    Code Preview
+                                </Link>
+                            </DivContainerText>
+                        </DivFlex>         
+                    </DivFlex>
+
+        </>
+    );
+}
+
+export default ProjectModel;
--- src/assets/components/ProjectsGrid/index.tsx
+++ src/assets/components/ProjectsGrid/index.tsx
@@ -1,9 +1,6 @@
 import { 
     DivContainerText, 
-    DivFlex, 
-    DivFlexImage, 
-    Img, 
-    Link, 
+    DivFlex,   
     Texto, 
     TitleH2 } from "../../../uiKit";
 
@@ -11,6 +8,7 @@ import BackGroundRemover from "../../images/backgroundremover.png"
 import Pco from "../../images/PCO.png"
 import link from "../../icons/link.svg";
 import Git from "../../icons/gitIconGrey.svg"
+import ProjectModel from "../ProjectModel";
 
 function ProjectGrid(){
     return(
@@ -63,241 +61,34 @@ function ProjectGrid(){
                     paddingTotal="20px 20px"
                     gap="40px"
                     borderradius="10px"
-                    background="radial-gradient(circle,#F5F5F5 15%, #E2E2E2 50%, #F5F5F5 85%)">
+                    background="radial-gradient(circle,#F5F5F5 15%, #E2E2E2 50%, #F5F5F5 85%)"
+                    flexdirectionmob="column">
 
                     {/* projeto background remover */}
 
-                <DivFlex 
-                        width="373px"                                                
-                        backgroundColor="#FFFFFF"
-                        flexdirection="column"
-                        paddingbottom="40px"  
-                        borderradius="10px"                      
-                        >
-                        <DivFlexImage>
-                            <Img
-                                borderradius="10px" 
-                                src={BackGroundRemover}/>
-                        </DivFlexImage>
-                        <DivContainerText
-                            flexdirection="column"
-                            heightTotal="100px"
-                            gap="20px"
-                            alignitems="center"
-                            justifycontent="center">
-                            <TitleH2
-                                color="#000000"
-                                fontsize="18px"
-                                fontweight="bold"
-                                fontfamily="Roboto"                        
-                                textalign="center">
-                                Background Remover
-                            </TitleH2>
-                            <Texto
-                                color="#666666"
-                                fontsize="16px"
-                                fontweight="400"
-                                fontfamily="Roboto"
-                                textalign="center"
-                                >
-                                Um projeto que utiliza API para remover o fundo de imagens de forma rápida e eficiente, Projeto feito com React e Typescript usando styled-components.
-                            </Texto>
-                    </DivContainerText>
-                    <DivContainerText
-                            flexdirection="row"
-                            gap="20px"
-                            alignitems="center"
-                            justifycontent="center"
-                            paddingtop="20px">
-                            <TitleH2
-                                color="#000000"
-                                fontsize="14px"
-                                fontweight="bold"
-                                fontfamily="Roboto"                        
-                                textalign="center">
-                                Tech Stack:
-                            </TitleH2>
-                            <Texto
-                                color="#666666"
-                                fontsize="14px"
-                                fontweight="400"
-                                fontfamily="Roboto"
-                                textalign="center"
-                                >
-                                React, Typescript, Styled-Components.
-                            </Texto>
-                    </DivContainerText>
-                        <DivFlex
-                            flexdirection="row"
-                            alignitems="center"
-                            justifycontent="center"
-                            gap="80px"
-                            >
-                            <DivContainerText
-                                flexdirection="row"
-                                gap="10px"
-                                alignitems="center"
-                                justifycontent="center"
-                                paddingtop="20px"                  
-                                >
-                                <Img 
-                                    width={"20px"} 
-                                    height={"20px"} 
-                                    alignitens="center"
-                                    src={link}/>
-                                <Link 
-                                    fontsize="14px"
-                                    fontweight="400"
-                                    fontfamily="Roboto"
-                                    color="#000000"
-                                    href="https://srb-psi.vercel.app/" 
-                                    target="_blank"
-                                    >                               
-                                    Live Preview
-                                </Link>
-                            </DivContainerText>
-                            <DivContainerText
-                                flexdirection="row"
-                                gap="10px"
-                                alignitems="center"
-                                justifycontent="center"
-                                paddingtop="20px"                  
-                                >
-                                <Img 
-                                    width={"20px"} 
-                                    height={"20px"} 
-                                    alignitens="center"
-                                    src={Git}/>
-                                <Link 
-                                    fontsize="14px"
-                                    fontweight="400"
-                                    fontfamily="Roboto"
-                                    color="#000000"
-                                    href="https://github.com/guiestrela/SRB"
-                                    target="_blank"
-                                    >                             
-                                    Code Preview
-                                </Link>
-                            </DivContainerText>
-                        </DivFlex>         
-                    </DivFlex>
+                <ProjectModel
+                    foto={BackGroundRemover}
+                    titulo="Background Remover"
+                    descricao="Um projeto que utiliza API para remover o fundo de imagens de forma rápida e eficiente, Projeto feito com React e Typescript usando styled-components."
+                    techStack="React, Typescript, Styled-Components"
+                    iconPreview={link}
+                    linkPreview="https://github.com/guiestrela/SRB"
+                    iconGit= {Git}
+                    linkGit="https://srb-psi.vercel.app/"
+                />
 
                     {/* projeto PCO */}
 
-                    <DivFlex 
-                        width="373px" 
-                        borderradius="10px"                         
-                        backgroundColor="#FFFFFF"
-                        flexdirection="column"
-                        paddingbottom="40px"                        
-                        >
-                        <DivFlexImage>
-                            <Img
-                                borderradius="10px" 
-                                src={Pco}/>
-                        </DivFlexImage>
-                        <DivContainerText
-                            flexdirection="column"
-                            gap="20px"
-                            heightTotal="100px"
-                            alignitems="center"
-                            justifycontent="center">
-                            <TitleH2
-                                color="#000000"
-                                fontsize="18px"
-                                fontweight="bold"
-                                fontfamily="Roboto"                        
-                                textalign="center">
-                                PCO
-                            </TitleH2>
-                            <Texto
-                                color="#666666"
-                                fontsize="16px"
-                                fontweight="400"
-                                fontfamily="Roboto"
-                                textalign="center"
-                                >
-                                Um projeto de uma site para uma ONG real que atua em Brasilia.
-                            </Texto>
-                    </DivContainerText>
-                    <DivContainerText
-                            flexdirection="row"
-                            gap="20px"
-                            alignitems="center"
-                            justifycontent="center"
-                            paddingtop="20px">
-                            <TitleH2
-                                color="#000000"
-                                fontsize="14px"
-                                fontweight="bold"
-                                fontfamily="Roboto"                        
-                                textalign="center">
-                                Tech Stack:
-                            </TitleH2>
-                            <Texto
-                                color="#666666"
-                                fontsize="14px"
-                                fontweight="400"
-                                fontfamily="Roboto"
-                                textalign="center"
-                                >
-                                React, Typescript, Styled-Components.
-                            </Texto>
-                    </DivContainerText>
-                        <DivFlex
-                            flexdirection="row"
-                            alignitems="center"
-                            justifycontent="center"
-                            gap="80px"
-                            >
-                            <DivContainerText
-                                flexdirection="row"
-                                gap="10px"
-                                alignitems="center"
-                                justifycontent="center"
-                                paddingtop="20px"                  
-                                >
-                                <Img 
-                                    width={"20px"} 
-                                    height={"20px"} 
-                                    alignitens="center"
-                                    src={link}/>
-                                <Link 
-                                    fontsize="14px"
-                                    fontweight="400"
-                                    fontfamily="Roboto"
-                                    color="#000000"
-                                    href="https://pco-seven.vercel.app/" 
-                                    target="_blank"
-                                    >                               
-                                    Live Preview
-                                </Link>
-                            </DivContainerText>
-                            <DivContainerText
-                                flexdirection="row"
-                                gap="10px"
-                                alignitems="center"
-                                justifycontent="center"
-                                paddingtop="20px"                  
-                                >
-                                <Img 
-                                    width={"20px"} 
-                                    height={"20px"} 
-                                    alignitens="center"
-                                    src={Git}/>
-                                <Link 
-                                    fontsize="14px"
-                                    fontweight="400"
-                                    fontfamily="Roboto"
-                                    color="#000000"
-                                    href="https://github.com/guiestrela/PCO"
-                                    target="_blank"
-                                    >                             
-                                    Code Preview
-                                </Link>
-                            </DivContainerText>
-                        </DivFlex>         
-                    </DivFlex>
+                <ProjectModel
+                    foto={Pco}
+                    titulo="PCO"
+                    descricao="Um projeto de uma site para uma ONG real que atua em Brasilia."
+                    techStack="React, Typescript, Styled-Components"
+                    iconPreview={link}
+                    linkPreview="https://pco-seven.vercel.app/"
+                    iconGit= {Git}
+                    linkGit="https://github.com/guiestrela/PCO"
+                />                    
                 </DivFlex>
                 
             </DivFlex>
