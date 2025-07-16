b6a659207d5e7a9cd8a65495c9454bc4ea9830c8
guilherme <guilhermestrela@gmail.com>
2025-07-16 15:07:48 -0300

    feat(Contato): add contact component
    - Added email and phone number
    - Implemented responsive design
    - Used styled-components for styling
    - Improved component structure
    - Enhanced UI/UX

--- src/assets/components/Contato/index.tsx
+++ src/assets/components/Contato/index.tsx
@@ -1,7 +1,57 @@
+import { DivContainerText, DivFlex, Texto, TitleH2 } from "../../../uiKit";
+
 function Contato() {
     return (
         <>
-            
+            <DivFlex
+                width="100%" 
+                alignitems="center" 
+                justifycontent="center">
+                <DivFlex
+                    width="1440px" 
+                    height="100%" 
+                    alignitems="center" 
+                    paddingTotal="180px 0"                         justifycontent="space-between"
+                    flexdirection="column">
+                    <DivContainerText
+                        flexdirection="column"
+                        paddingTotal="0 20px">
+                        <TitleH2
+                            fontfamily="Roboto"
+                            fontsize="38px"
+                            fontweight="700"
+                            textalign="center"                                color="#42446e">
+                            Email:
+                        </TitleH2>
+                        <Texto
+                            fontfamily="Roboto"
+                            fontsize="30px"
+                            fontweight="400"
+                            textalign="start"
+                            color="#666666"                                paddingtop="10px">                                 guilhermestrela@gmail.com
+                        </Texto>
+                    </DivContainerText>
+                    <DivContainerText
+                        flexdirection="column"
+                        paddingTotal="80px 20px 0">
+                        <TitleH2
+                            fontfamily="Roboto"
+                            fontsize="38px"
+                            fontweight="700"
+                            textalign="center"                                color="#42446e">
+                            Telefone:
+                        </TitleH2>
+                        <Texto
+                            fontfamily="Roboto"
+                            fontsize="30px"
+                            fontweight="400"
+                            textalign="start"
+                            color="#666666"                                paddingtop="10px">                                 
+                            +55 (61) 99830-6076
+                        </Texto>
+                    </DivContainerText>                                 
+                </DivFlex>
+            </DivFlex> 
         </>
     );
 }
